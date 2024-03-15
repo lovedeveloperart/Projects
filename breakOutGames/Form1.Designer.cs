c7cce844d1409256cff95076c19650628f77f5af
PEERAPON NANTAPAN <artpeerapon3@gmail.com>
2024-03-16 01:42:02 +0700

    Add project files.

---
+++ breakOutGames/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace breakOutGames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictPaddle = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictPaddle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.Maroon;
+            this.picBall.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picBall.Location = new System.Drawing.Point(287, 399);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(25, 25);
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictPaddle
+            // 
+            this.pictPaddle.BackColor = System.Drawing.Color.Cornsilk;
+            this.pictPaddle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictPaddle.Location = new System.Drawing.Point(247, 471);
+            this.pictPaddle.Name = "pictPaddle";
+            this.pictPaddle.Size = new System.Drawing.Size(100, 25);
+            this.pictPaddle.TabIndex = 1;
+            this.pictPaddle.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.pictPaddle);
+            this.Controls.Add(this.picBall);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictPaddle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictPaddle;
+    }
+}
+
---
+++ breakOutGames/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace breakOutGames
+{
+    public partial class Form1 : Form
+    {
+        public int vSpeed;
+        public int hSpeed;
+        public const int row = 5;
+        public const int col = 6;
+        public PictureBox[,] blocks; 
+        //constructor
+
+        public Form1()
+        {
+            vSpeed = 3;
+            hSpeed = 3;
+            setBlocks();
+            InitializeComponent();
+           
+        }
+
+        private void setBlocks()
+        {
+            int blockHeight = 25;
+            int blockWidth = 100;
+
+            blocks = new PictureBox[row, col];
+
+            for(int x = 0;x  < row; x++)
+            {
+                for(int  y = 0; y < col; y++)
+                {
+                    blocks[x, y] = new PictureBox();
+
+                    blocks[x, y].Width = blockWidth;
+                    blocks[x, y].Height = blockHeight;
+                    blocks[x, y].Top = blockHeight * x;
+                    blocks[x, y].Left = blockHeight * y;
+                    blocks[x, y].BackColor = Color.Green;
+                    blocks[x, y].BorderStyle = BorderStyle.Fixed3D;
+
+                    this.Controls.Add(blocks[x, y]);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //Method Control Ball
+        {
+            picBall.Top += vSpeed;
+            picBall.Left += hSpeed;
+
+            if(picBall.Bottom > this.ClientSize.Height) //เงื่อไขที่ทำให้บอลไม่ออกจากแมพเด้งอยู่แค่ในหน้าwindow
+            {
+                vSpeed = -vSpeed;
+            }
+
+            if(picBall.Top < 0)
+            {
+                vSpeed = -vSpeed;
+            }
+
+            if(picBall.Right > this.ClientSize.Width)
+            {
+                hSpeed = -hSpeed;
+            }
+
+            if(picBall.Left < 0)
+            {
+                hSpeed = -hSpeed;
+            }
+
+            //When Ball Collides with paddle
+            if (picBall.Bounds.IntersectsWith(pictPaddle.Bounds) ==  true )
+            {
+                vSpeed = -vSpeed;
+                //hSpeed = -hSpeed;
+            }
+
+            //detect collision with blocks
+            for(int x = 0; x < row; x++)
+            {
+                for(int y = 0; y < col; y++)
+                {
+                    if (picBall.Bounds.IntersectsWith(blocks[x, y].Bounds)&& blocks[x,y].Visible == true)
+                    {
+                        blocks[x, y].Visible = false;
+                        vSpeed = -vSpeed;
+                    }
+                }
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            pictPaddle.Left = e.X - (pictPaddle.Width / 2);
+        }
+    }
+}
